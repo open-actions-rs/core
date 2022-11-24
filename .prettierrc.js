9ddee08550fcce2c341af86424bb2570cef59aef
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-11-24 15:48:40 -0800

    fix: cleanup

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,30 +1,31 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    "arrowParens": "always",
-    "bracketSpacing": true,
-    "printWidth": 200,
-    "quoteProps": "as-needed",
-    "semi": true,
-    "singleQuote": false,
-    "useTabs": false,
-    "tabWidth": 4,
-    "trailingComma": "all",
-    "overrides": [
+    arrowParens: "always",
+    bracketSpacing: true,
+    printWidth: 200,
+    quoteProps: "as-needed",
+    semi: true,
+    singleQuote: false,
+    useTabs: false,
+    tabWidth: 4,
+    trailingComma: "all",
+    overrides: [
         {
-            "files": "*.js",
-            "options": {
-                "parser": "babel",
+            files: "*.js",
+            options: {
+                parser: "babel",
             },
         },
         {
-            "files": "*.json",
-            "options": {
-                "parser": "json",
+            files: "*.json",
+            options: {
+                parser: "json",
             },
         },
         {
-            "files": "*.ts",
-            "options": {
-                "parser": "typescript",
+            files: "*.ts",
+            options: {
+                parser: "typescript",
             },
         },
     ],
